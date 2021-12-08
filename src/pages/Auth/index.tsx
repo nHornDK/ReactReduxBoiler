95ced3f5b8f59b3d53ba346733c27fc83eb62f5d
nhorn <mikkel@nhorn.dk>
2021-12-08 07:55:29 +0000

    cleanup

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,42 +1,15 @@
-/* eslint-disable react/no-unused-class-component-methods */
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import Container from '@fluffy-spoon/inverse';
 import React from 'react';
 import { connect, ConnectedProps } from 'react-redux';
+import { Button } from '../../components/ui';
 
-import { RoundedBox } from '../../components/ui';
 import { GoogleAuthenticationService } from '../../infrastructure/services';
-import { GevirNoteDocument } from '../../models/firestore/GevirNoteDocument';
 import actions from '../../store/Actions';
 import RootState from '../../store/RootState';
 
 type AuthProps = ConnectedProps<typeof connector>;
 
-interface AuthState {
-	gevirNotes: GevirNoteDocument[];
-}
-class AuthPage extends React.Component<AuthProps, AuthState> {
-	private authenticationService: GoogleAuthenticationService;
-
-	constructor(props: AuthProps) {
-		super(props);
-		this.state = {
-			gevirNotes: [],
-		};
-		const container = new Container();
-		this.authenticationService = container.resolveInstance(GoogleAuthenticationService);
-	}
-
+class AuthPage extends React.Component<AuthProps> {
 	componentDidMount(): void {
-		console.log('componentDidMount', { props: this.props });
-		// try {
-		// 	// const notes = await this.firestoreService.getGevirNotes();
-		// 	this.setState({ gevirNotes: notes?.documents ?? [] });
-		// 	console.log({ notes });
-		// } catch (error: unknown) {
-		// 	console.error(error);
-		// }
 		this.handleAuthCallback();
 	}
 
@@ -60,33 +33,16 @@ class AuthPage extends React.Component<AuthProps, AuthState> {
 	};
 
 	render(): JSX.Element {
-		const { gevirNotes } = this.state;
 		const {
 			authenticate: { token },
 		} = this.props;
-		console.log('render', { token });
-		// if (/* this.IsAuthenticated() && authenticate.userInfo */ false) {
-		// 	return <Redirect to={'/'} />;
-		// }
 		return (
 			<div>
 				<h1>Auth page</h1>
 				<p>access_token: {token?.access_token}</p>
-				<button type='button' onClick={this.handleClickAuth}>
+				<Button variant='primary' onClick={this.handleClickAuth}>
 					Login to your google account
-				</button>
-				{gevirNotes.map((n) => (
-					<RoundedBox key={n.name}>
-						<b>{n.name}</b>
-						<br />
-						Title2:
-						{n.fields.Title.stringValue}
-						<br />
-						Note:
-						{n.fields.Note.stringValue}
-						<br />
-					</RoundedBox>
-				))}
+				</Button>
 			</div>
 		);
 	}
