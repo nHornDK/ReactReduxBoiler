a2d65c75ca2867574f16e2c73abde7b427ce0071
Mikkel.Halgaard <mhalgaard94@gmail.com>
2021-12-08 10:58:46 +0000

    Initial setup of Delete + implemented Update in redux

--- src/store/todo/TodoActions.ts
+++ src/store/todo/TodoActions.ts
@@ -1,4 +1,5 @@
 import Container from '@fluffy-spoon/inverse';
+import { Action } from 'redux';
 
 import { GoogleFirestoreDocumentService } from '../../infrastructure/services';
 import { TodoDocument } from '../../models/firestore';
@@ -7,8 +8,10 @@ import { ActionType, AppThunk, PayloadAction } from '../StoreTypes';
 
 export type TodoListAction = PayloadAction<FirestoreDocuments<TodoDocument>, ActionType.TODO_LIST_SUCCESS>;
 export type TodoCreateAction = PayloadAction<TodoDocument, ActionType.TODO_CREATE_SUCCESS>;
+export type TodoUpdateAction = PayloadAction<TodoDocument, ActionType.TODO_UPDATE_SUCCESS>;
+export type TodoDeleteAction = PayloadAction<string, ActionType.TODO_DELETE_SUCCESS>;
 export type TodoFailedAction = PayloadAction<unknown, ActionType.TODO_FAILED>;
-export type TodoActions = TodoListAction | TodoCreateAction | TodoFailedAction;
+export type TodoActions = TodoListAction | TodoCreateAction | TodoUpdateAction | TodoDeleteAction | TodoFailedAction;
 
 export default class TodoDispatchers {
 	private documentService: GoogleFirestoreDocumentService;
@@ -77,4 +80,38 @@ export default class TodoDispatchers {
 				},
 			});
 		};
+
+	public todoNoteUpdate =
+		(item: Partial<TodoDocument>): AppThunk<Promise<TodoUpdateAction | TodoFailedAction>> =>
+		async (dispatch, getState): Promise<TodoUpdateAction | TodoFailedAction> => {
+			try {
+				const { authenticate } = getState();
+				if (authenticate && authenticate.token) {
+					const response = await this.documentService.updateDocument<Partial<TodoDocument>, TodoDocument>('TodoItems', item, authenticate.token);
+					return dispatch({
+						type: ActionType.TODO_UPDATE_SUCCESS,
+						payload: {
+							data: response,
+						},
+					});
+				}
+			} catch (error: unknown) {
+				return dispatch({
+					type: ActionType.TODO_FAILED,
+					payload: {
+						data: error,
+					},
+				});
+			}
+			return dispatch({
+				type: ActionType.TODO_FAILED,
+				payload: {
+					data: 'YEET',
+				},
+			});
+		};
+
+	// public todoNoteDelete = () => {
+
+	// }
 }
